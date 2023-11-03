0b836377b0fb40be426623b47b33aa02c9b8b12f
clément <clemkfr@gmail.com>
2023-11-03 11:53:00 +0100

    r

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ fetch(`https://deploymetovercel-liard.vercel.app/year`)
 // fetch('http://localhost:3000/year')
 .then(response => response.json())
 .then((data)=>{
-   area.textContent = data.yea
+   area.textContent = data.year
 })
