15c47107d2d4dfe6342b14c38e3710f3596a9832
clément <clemkfr@gmail.com>
2023-11-03 11:28:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+let area = document.querySelector('#year')
+fetch('https://deploymetovercel-liard.vercel.app/year')
+// fetch('http://localhost:3000/year')
+.then(response => response.json())
+.then((data)=>{
+   area.textContent = JSON.stringify(data)
+})
