9faab062052d57f212c78f9d715f33e4ea636e85
clément <clemkfr@gmail.com>
2023-11-03 11:51:32 +0100

    good-form

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 let area = document.querySelector('#year')
-fetch('https://deploymetovercel-liard.vercel.app/year')
+fetch(`https://deploymetovercel-liard.vercel.app/year`)
 // fetch('http://localhost:3000/year')
 .then(response => response.json())
 .then((data)=>{
-   area.textContent = JSON.stringify(data)
+   area.textContent = data.yea
 })
